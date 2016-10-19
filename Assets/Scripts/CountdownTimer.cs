77d10ac1e63f992ecfc4f0086aa04dbb046b37b4
Miranda Breekweg <mirandabreekweg@live.nl>
2016-10-19 23:45:04 +0200

    UI scene with countdown timer

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour {
+	public float timeLeft = 300.0f;
+	private Text timerText;
+	private float startTime;
+
+	private int minutes;
+	private int seconds;
+
+	void Start () {
+		startTime = Time.time;
+		timerText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeLeft > 1) {
+			timeLeft -= Time.deltaTime;
+
+			minutes = ((int)timeLeft / 60);
+			seconds = ((int)timeLeft % 60);
+
+			timerText.text = string.Format ("{0:00}:{1:00}", minutes, seconds);
+		} else {
+			timerText.text = "Game over!";
+		}
+	}
+}
